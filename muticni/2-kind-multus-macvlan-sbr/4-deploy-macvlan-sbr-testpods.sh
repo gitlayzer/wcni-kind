5179fdf0da9dd7e831bb06c3e552e1a354145970
rowan <olaf.luo@foxmail.com>
2022-12-15 13:15:50 +0800

    add canal

--- muticni/2-kind-multus-macvlan-sbr/4-deploy-macvlan-sbr-testpods.sh
+++ muticni/2-kind-multus-macvlan-sbr/4-deploy-macvlan-sbr-testpods.sh
@@ -8,11 +8,11 @@ cat <<EOF | kubectl apply -f -
 apiVersion: "k8s.cni.cncf.io/v1"
 kind: NetworkAttachmentDefinition
 metadata:
-  name: macvlan-whereabouts-conf
+  name: macvlan-whereabouts-conf-1
 spec:
   config: '{
     "cniVersion": "0.3.1",
-    "name": "macvlan-whereabouts-conf",
+    "name": "macvlan-whereabouts-conf-1",
     "plugins": [
       {
         "type": "macvlan",
@@ -34,6 +34,36 @@ spec:
   }'
 EOF
 
+cat <<EOF | kubectl apply -f -
+apiVersion: "k8s.cni.cncf.io/v1"
+kind: NetworkAttachmentDefinition
+metadata:
+  name: macvlan-whereabouts-conf-2
+spec:
+  config: '{
+    "cniVersion": "0.3.1",
+    "name": "macvlan-whereabouts-conf-2",
+    "plugins": [
+      {
+        "type": "macvlan",
+        "master": "net0",
+        "mode": "bridge",
+        "ipam": {
+          "type": "whereabouts",
+          "range": "16.1.5.2-16.1.5.100/24",
+          "gateway": "16.1.5.1",
+          "route": [
+            { "dst": "0.0.0.0/0"
+          }]
+        }
+      },
+      {
+        "type": "sbr"
+      }
+    ]
+  }'
+EOF
+
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -41,7 +71,7 @@ kind: Pod
 metadata:
   name: macvlan-sbr-pod1
   annotations:
-    k8s.v1.cni.cncf.io/networks: macvlan-whereabouts-conf@eth1
+    k8s.v1.cni.cncf.io/networks: macvlan-whereabouts-conf-1@eth1,macvlan-whereabouts-conf-2@eth2
 spec:
   containers:
   - name: nettool
@@ -59,7 +89,7 @@ kind: Pod
 metadata:
   name: macvlan-sbr-pod2
   annotations:
-    k8s.v1.cni.cncf.io/networks: macvlan-whereabouts-conf@eth1
+    k8s.v1.cni.cncf.io/networks: macvlan-whereabouts-conf-1@eth1,macvlan-whereabouts-conf-2@eth2
 spec:
   containers:
   - name: nettool
