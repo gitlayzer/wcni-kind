e38a0096f073cc58f121c2b16d815f1900f114e2
rowan <olaf.luo@foxmail.com>
2022-12-28 16:49:13 +0800

    add

--- muticni/3-kind-multus-ipvlanl2/1-setup-env.sh
+++ muticni/3-kind-multus-ipvlanl2/1-setup-env.sh
@@ -28,14 +28,3 @@ kubectl get nodes -o wide
 # 3. install CNI
 kubectl apply -f calico.yaml
 
-# 4. install necessary tools
-for i in $(docker ps  -a --format "table {{.Names}}" | grep calico-ipip)
-do 
-    echo $i
-    docker cp /usr/bin/calicoctl $i:/usr/bin/calicoctl
-    docker cp ./ipvlan $i:/opt/cni/bin/
-    docker cp /usr/bin/ping $i:/usr/bin/ping
-    docker exec -it $i bash -c "sed -i -e 's/jp.archive.ubuntu.com\|archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list"
-    docker exec -it $i bash -c "apt-get -y update >/dev/null && apt-get -y install net-tools tcpdump lrzsz >/dev/null 2>&1"
-done
-
