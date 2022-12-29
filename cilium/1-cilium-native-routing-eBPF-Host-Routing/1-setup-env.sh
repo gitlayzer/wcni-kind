b8fe6731d982f7448f3281970e1f3402b32444f6
rowan <olaf.luo@foxmail.com>
2022-12-29 19:31:54 +0800

    mod cilium

--- cilium/1-cilium-native-routing-eBPF-Host-Routing/1-setup-env.sh
+++ cilium/1-cilium-native-routing-eBPF-Host-Routing/1-setup-env.sh
@@ -28,7 +28,7 @@ kubectl get nodes -o wide
 
 # 3. install CNI
 helm repo add cilium https://helm.cilium.io > /dev/null 2>&1
-helm install cilium cilium/cilium --set k8sServiceHost=$controller_node --set k8sServicePort=6443 --version 1.12.0 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-native-routing-host-routing --set kubeProxyReplacement=strict --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set tunnel=disabled --set bpf.masquerade=true --set installNoConntrackIptablesRules=true
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node --set k8sServicePort=6443 --version 1.12.0 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-native-routing-host-routing --set kubeProxyReplacement=strict --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set tunnel=disabled --set bpf.masquerade=true --set installNoConntrackIptablesRules=true --set loadBalancer.mode=dsr
 
 # 4. install necessary tools
 for i in $(docker ps  -a --format "table {{.Names}}" | grep cilium-native-routing-host-routing)
