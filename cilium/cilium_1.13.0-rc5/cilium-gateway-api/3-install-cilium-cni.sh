3ef8fdaa4e839feed5efd686b2160c2194390693
BurlyLuo <olaf.luo@foxmail.com>
2023-02-18 00:01:22 +0800

    adapt kind and kubeadm

--- cilium/cilium_1.13.0-rc5/cilium-gateway-api/3-install-cilium-cni.sh
+++ cilium/cilium_1.13.0-rc5/cilium-gateway-api/3-install-cilium-cni.sh
@@ -1,14 +1,8 @@
 #/bin/bash
-
-controller_node=`kubectl get nodes --no-headers  -o custom-columns=NAME:.metadata.name | grep -E "control-plane|bpf1"`
-if [[ $controller_node == "bpf1" ]]
-then
-   controller_node="192.168.2.71"
-else
-   controller_node="controller_node"
-fi
+controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
 
 helm repo add cilium https://helm.cilium.io > /dev/null 2>&1
 helm repo update > /dev/null 2>&1
-helm install cilium cilium/cilium --set k8sServiceHost=$controller_node --set k8sServicePort=6443 --version 1.13.0-rc5 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=socketlb --set kubeProxyReplacement=strict --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set tunnel=disabled --set bpf.masquerade=true --set installNoConntrackIptablesRules=true --set gatewayAPI.enabled=true
+
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.13.0-rc5 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=socketlb --set kubeProxyReplacement=strict --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set tunnel=disabled --set bpf.masquerade=true --set installNoConntrackIptablesRules=true --set gatewayAPI.enabled=true
 
