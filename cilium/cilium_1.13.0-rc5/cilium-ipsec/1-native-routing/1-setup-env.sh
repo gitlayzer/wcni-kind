3756155b59cc69a17cc646813589aa56bdb8743a
rowan <olaf.luo@foxmail.com>
2023-03-02 23:29:32 +0800

    add ipsec with vxlan-native

--- cilium/cilium_1.13.0-rc5/cilium-ipsec/1-native-routing/1-setup-env.sh
+++ cilium/cilium_1.13.0-rc5/cilium-ipsec/1-native-routing/1-setup-env.sh
@@ -3,12 +3,12 @@ date
 set -v
 
 # 1.prep noCNI env
-cat <<EOF | kind create cluster --name=cilium-ipsec --image=kindest/node:v1.23.4 --config=-
+cat <<EOF | kind create cluster --name=cilium-ipsec-native-routing --image=kindest/node:v1.23.4 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
         disableDefaultCNI: true
-        #kubeProxyMode: "none" # do not comment out this feature! otherwise, it will cause the service abnomal.
+        #kubeProxyMode: "none" # Enable the kubeProxy
 nodes:
         - role: control-plane
         - role: worker
@@ -31,5 +31,5 @@ helm repo add cilium https://helm.cilium.io > /dev/null 2>&1
 kubectl create -n kube-system secret generic cilium-ipsec-keys \
 	    --from-literal=keys="3 rfc4106(gcm(aes)) $(echo $(dd if=/dev/urandom count=20 bs=1 2> /dev/null | xxd -p -c 64)) 128"
 
-helm install cilium cilium/cilium --set k8sServiceHost=$controller_node --set k8sServicePort=6443 --version 1.13.0-rc5 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-ipsec --set encryption.enabled=true --set encryption.type=ipsec
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node --set k8sServicePort=6443 --version 1.13.0-rc5 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-ipsec-native-routing --set tunnel=disabled --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set encryption.enabled=true --set encryption.type=ipsec
 
