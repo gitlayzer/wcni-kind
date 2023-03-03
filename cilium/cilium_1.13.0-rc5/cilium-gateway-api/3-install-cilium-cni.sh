27277d59860920f8683d5e58946044e919f76963
rowan <olaf.luo@foxmail.com>
2023-03-03 15:26:04 +0800

    add cilium-v1.13.0-rc5 gateway-api

--- cilium/cilium_1.13.0-rc5/cilium-gateway-api/3-install-cilium-cni.sh
+++ cilium/cilium_1.13.0-rc5/cilium-gateway-api/3-install-cilium-cni.sh
@@ -4,7 +4,5 @@ controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-pla
 helm repo add cilium https://helm.cilium.io > /dev/null 2>&1
 helm repo update > /dev/null 2>&1
 
-# please note: The cilium-gateway-api feature need metallb with v0.12.1. otherwise it will cause "No route ro the host!"
-# https://github.com/cilium/cilium/issues/24088
-helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.13.0-rc5 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=socketlb --set kubeProxyReplacement=strict --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set tunnel=disabled --set bpf.masquerade=true --set installNoConntrackIptablesRules=true --set gatewayAPI.enabled=true
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.13.0-rc5 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-socketlb --set kubeProxyReplacement=strict --set bpf.masquerade=true --set gatewayAPI.enabled=true
 
