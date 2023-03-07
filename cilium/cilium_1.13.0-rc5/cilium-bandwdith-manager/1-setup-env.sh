da47400951ee8e4ff8bb4934b7ded46b63d179f7
rowan <olaf.luo@foxmail.com>
2023-03-07 11:31:05 +0800

    stand the cilium 1-setup-env.sh

--- cilium/cilium_1.13.0-rc5/cilium-bandwdith-manager/1-setup-env.sh
+++ cilium/cilium_1.13.0-rc5/cilium-bandwdith-manager/1-setup-env.sh
@@ -28,6 +28,7 @@ kubectl get nodes -o wide
 # 3. install CNI
 helm repo add cilium https://helm.cilium.io > /dev/null 2>&1
 helm repo update > /dev/null 2>&1
+
 helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.13.0-rc5 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-vxlan-bandwidth-manager --set kubeProxyReplacement=strict --set bpf.masquerade=true --set bandwidthManager.enabled=true 
 
 
--- cilium/cilium_1.13.0-rc5/cilium-ipsec/1-native-routing/1-setup-env.sh
+++ cilium/cilium_1.13.0-rc5/cilium-ipsec/1-native-routing/1-setup-env.sh
@@ -21,15 +21,16 @@ containerdConfigPatches:
 EOF
 
 # 2.remove taints
-controller_node=`kubectl get nodes --no-headers  -o custom-columns=NAME:.metadata.name| grep control-plane`
-kubectl taint nodes $controller_node node-role.kubernetes.io/master:NoSchedule-
+controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
+kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.kubernetes.io/master:NoSchedule-
 kubectl get nodes -o wide
 
 # 3. install CNI
 helm repo add cilium https://helm.cilium.io > /dev/null 2>&1
+helm repo update > /dev/null 2>&1
 
 kubectl create -n kube-system secret generic cilium-ipsec-keys \
 	    --from-literal=keys="3 rfc4106(gcm(aes)) $(echo $(dd if=/dev/urandom count=20 bs=1 2> /dev/null | xxd -p -c 64)) 128"
 
-helm install cilium cilium/cilium --set k8sServiceHost=$controller_node --set k8sServicePort=6443 --version 1.13.0-rc5 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-ipsec-native-routing --set tunnel=disabled --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set encryption.enabled=true --set encryption.type=ipsec
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.13.0-rc5 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-ipsec-native-routing --set tunnel=disabled --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set encryption.enabled=true --set encryption.type=ipsec
 
