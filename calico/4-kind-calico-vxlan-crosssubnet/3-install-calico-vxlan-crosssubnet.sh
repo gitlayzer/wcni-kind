a590a631d9834fc32aa8b050ae1f84f9a0b5537a
BurlyLuo <olaf.luo@foxmail.com>
2023-05-27 20:32:17 +0800

    enlarge wait timer

--- calico/4-kind-calico-vxlan-crosssubnet/3-install-calico-vxlan-crosssubnet.sh
+++ calico/4-kind-calico-vxlan-crosssubnet/3-install-calico-vxlan-crosssubnet.sh
@@ -3,4 +3,5 @@
 kubectl apply -f calico.yaml
 
 # 2. wait all pods ready
-kubectl wait --timeout=45s --for=condition=Ready=true pods --all -A
+kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
+
