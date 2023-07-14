f6996353c1a10b92ee136cd59e0240974174117c
rowan <olaf.luo@foxmail.com>
2023-07-14 22:36:23 +0800

    add cilium l2 aware lb

--- split/cilium/cilium-l2-aware-lb/1-setup-env.sh
+++ split/cilium/cilium-l2-aware-lb/1-setup-env.sh
@@ -33,8 +33,8 @@ helm repo update > /dev/null 2>&1
 # Direct Routing Options(--set tunnel=disabled --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8")
 # kubeproxyreplacement Options(--set kubeProxyReplacement=true)
 # eBPF Host Routing(--set bpf.masquerade=true)
-# L2 Aware LB(--set l2announcements.enabled=true)
-helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.14.0-rc.0 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-l2-aware-lb --set kubeProxyReplacement=true --set tunnel=disabled --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8" --set bpf.masquerade=true --set l2announcements.enabled=true --set devices='{eth0, net0}' --set externalIPs.enabled=true
+# L2 Aware LB(--set l2announcements.enabled=true --set devices='{eth0}' --set externalIPs.enabled=true)
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.14.0-rc.0 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-l2-aware-lb --set kubeProxyReplacement=true --set tunnel=disabled --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8" --set l2announcements.enabled=true --set devices='{eth0}' --set externalIPs.enabled=true
 
 # 4. wait all pods ready
 kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
@@ -44,4 +44,9 @@ kubectl -nkube-system exec -it ds/cilium -- cilium status
 
 # 6. cgroup v2 verify
 for container in $(docker ps  -a --format "table {{.Names}}" | grep cilium-l2-aware-lb);do docker exec $container ls -al /proc/self/ns/cgroup;done
+
+# 7. issue list
+# [L2 announcement] https://github.com/cilium/cilium/pull/25471
+# [L2 announcements stop working after a time] https://github.com/cilium/cilium/issues/26586
+# [CFP: Support PROXY Protocol from L2 Aware LB and CNI] https://github.com/cilium/cilium/issues/26530
 
