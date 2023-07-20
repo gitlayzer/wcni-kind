fdb0ea06c76ecde5f50bd786ae3443c748d42fa9
rowan <olaf.luo@foxmail.com>
2023-07-20 21:41:31 +0800

    add multipass ubuntu2304

--- split/cilium/multipass/ubuntu2304/1-setup-env.sh
+++ split/cilium/multipass/ubuntu2304/1-setup-env.sh
@@ -2,22 +2,21 @@
 set -v
 
 # 1. Deploy multipass vm
-multipass stop --all;multipass delete --all;multipass purge;multipass list;sed -i '1!d' /root/.ssh/known_hosts > /dev/null 2>&1
-kubectl config delete-context k3s-ha > /dev/null 2>&1
+multipass stop --all;multipass delete --purge --all;sed -i '1!d' /root/.ssh/known_hosts > /dev/null 2>&1;kubectl config delete-context k3s-ha > /dev/null 2>&1
 
 for node in k3s-master0 k3s-worker1 k3s-worker2
 do
-  multipass launch 22.04 -n $node -c 2 -m 2G -d 10G --cloud-init - <<'EOF'
+  multipass launch 23.04 -n $node -c 2 -m 2G -d 10G --cloud-init - <<'EOF'
   # cloud-config
   runcmd:
-    - sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+    - sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config.d/*.conf
     - sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
     - echo 'root:hive' | sudo chpasswd
-    - sudo systemctl restart sshd
+    - sudo systemctl restart ssh
 EOF
 done
 
-# 2. prep env[ubuntu 22.04]
+# 2. prep env[ubuntu 23.04]
 mapfile -t ip_addresses < <(multipass list | grep -E 'k3s-master0|k3s-worker1|k3s-worker2' | awk '{print $3}')
 
 for ip_id in 0 1 2;do sshpass -p hive ssh-copy-id -o StrictHostKeyChecking=no -p 22 root@${ip_addresses[$ip_id]} > /dev/null 2>&1;done
@@ -35,8 +34,8 @@ helm repo update > /dev/null 2>&1
 # Direct Routing Options(--set tunnel=disabled --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8")
 # kubeproxyreplacement Options(--set kubeProxyReplacement=true)
 # eBPF Host Routing(--set bpf.masquerade=true)
-# bandwidthManager(--set bandwidthManager.enabled=true)
-helm install cilium cilium/cilium --set k8sServiceHost=${ip_addresses[0]} --set k8sServicePort=6443 --version 1.14.0-rc.0 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-bandwidth-manager --set kubeProxyReplacement=true --set tunnel=disabled --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8" --set bandwidthManager.enabled=true 
+# bbr(--set bandwidthManager.enabled=true --set bandwidthManager.bbr=true --set bpf.masquerade=true)[--set bpf.masquerade=true required the feature]
+helm install cilium cilium/cilium --set k8sServiceHost=${ip_addresses[0]} --set k8sServicePort=6443 --version 1.14.0-rc.0 --namespace kube-system --set debug.enabled=true --set debug.verbose=datapath --set monitorAggregation=none --set ipam.mode=cluster-pool --set cluster.name=cilium-bbr --set kubeProxyReplacement=true --set tunnel=disabled --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8" --set bandwidthManager.enabled=true --set bandwidthManager.bbr=true --set bpf.masquerade=true 
 
 # 4. wait all pods ready
 kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
