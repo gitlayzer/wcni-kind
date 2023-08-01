da1d366702c01cd1f63205bdc84eae60463e698c
rowan <olaf.luo@foxmail.com>
2023-08-01 10:57:53 +0800

    add create muti-vms script

---
+++ split/cilium/multipass/vm-ubuntu22.04/1-setup-env.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+set -v
+
+# 1. Deploy multipass vm
+multipass stop --all;multipass delete --purge --all;{ sed -i '1!d' /root/.ssh/known_hosts && kubectl config delete-context k3s-ha; } > /dev/null 2>&1
+
+for ((i=0; i<${1:-1}; i++))
+do
+  multipass launch 22.04 -n vm"$i" -c 2 -m 2G -d 10G --cloud-init - <<EOF
+  # cloud-config
+  runcmd:
+    - sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+    - sudo echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+    - echo 'root:hive' | sudo chpasswd
+    - sudo systemctl restart sshd
+EOF
+done
+
+# 2. prep env[ubuntu 22.04]
+mapfile -t ip_addresses < <(multipass list | grep -E 'vm' | awk '{print $3}')
+
+for ((ip_id=0; ip_id<${#ip_addresses[@]}; ip_id++));do sshpass -p hive ssh-copy-id -o StrictHostKeyChecking=no -p 22 root@${ip_addresses[$ip_id]} > /dev/null 2>&1;done
+
