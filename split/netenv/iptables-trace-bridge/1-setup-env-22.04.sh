3d698146309ea0ba080909fce0c3f50c7758e2a4
rowan <olaf.luo@foxmail.com>
2023-09-06 17:03:11 +0800

    add iptables trace log netenv

--- split/netenv/iptables-trace-bridge/1-setup-env-22.04.sh
+++ split/netenv/iptables-trace-bridge/1-setup-env-22.04.sh
@@ -5,10 +5,10 @@ if [ ${1:-1} -ge 2 ]; then cpu=2;mem=2G;else cpu=8;mem=8G;fi
 
 for ((i=0; i<${1:-1}; i++))
 do
-  multipass launch 20.04 -n vm"$i" -c $cpu -m $mem -d 30G --cloud-init - <<EOF
+  multipass launch 22.04 -n vm2204"$i" -c $cpu -m $mem -d 30G --cloud-init - <<EOF
   # cloud-config
   runcmd:
-    - sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config 
+    - sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
     - sudo echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config && echo 'root:hive' | sudo chpasswd && systemctl restart ssh
     - sudo bash -c '{ echo "alias all=\"kubectl get pods -A\""; echo "alias k=\"kubectl\""; echo "alias kk=\"kubectl -nkube-system\"" ; } >> ~/.bashrc'
     - sudo bash -c '{ echo "alias cls=\"kind get clusters\""; echo "alias cld=\"kind delete clusters\""; } >> ~/.bashrc'
@@ -23,14 +23,15 @@ do
     - sudo wget http://192.168.2.100/k3s/cilium-related/daemon.json -P /etc/docker/
     - sudo systemctl daemon-reload && systemctl restart docker && systemctl enable docker
     - sudo docker pull 192.168.2.100:5000/kindest:v1.27.3 && docker tag 192.168.2.100:5000/kindest:v1.27.3 kindest/node:v1.27.3
-    - sudo wget -r -np -nH --cut-dirs=2 --directory-prefix=/root/ http://192.168.2.100/k3s/vmenv/kindenv/ && rm -rf /root/kindenv/index.html
+    - sudo wget -r -np -nH --cut-dirs=3 --directory-prefix="/root/" http://192.168.2.100/k3s/vmenv/mmenv/ubuntu2204/ && chmod +x "/root/ubuntu2204/pwru.sh" 
+    - sudo find /root/ubuntu2204/ -name index.html -exec rm {} \;
 EOF
 done
 
-mapfile -t ip_addresses < <(multipass list | grep vm[0-9] | awk '{print $3}')
+mapfile -t ip_addresses < <(multipass list | grep vm2204[0-9] | awk '{print $3}')
 
 for ((ip_id=0; ip_id<${#ip_addresses[@]}; ip_id++)); do
     sshpass -p hive ssh-copy-id -o StrictHostKeyChecking=no -p 22 root@${ip_addresses[$ip_id]} > /dev/null 2>&1
-    echo "${ip_addresses[$ip_id]} vm$ip_id" >> /etc/hosts
+    echo "${ip_addresses[$ip_id]} vm2204$ip_id" >> /etc/hosts
 done
 
