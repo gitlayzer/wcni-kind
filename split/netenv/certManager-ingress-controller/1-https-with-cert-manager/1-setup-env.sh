dbbafac66fa7b7cc536d5c47a6ce7ff7108e5648
rowan <olaf.luo@foxmail.com>
2023-09-14 21:45:47 +0800

    wei

--- split/netenv/certManager-ingress-controller/1-https-with-cert-manager/1-setup-env.sh
+++ split/netenv/certManager-ingress-controller/1-https-with-cert-manager/1-setup-env.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-date
 set -v
-
 # 1.prep noCNI env
-cat <<EOF | kind create cluster --name=ingress-control-tls --image=kindest/node:v1.23.4 --config=-
+cat <<EOF | kind create cluster --name=ingress-control-https --image=kindest/node:v1.27.3 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
@@ -21,9 +19,13 @@ containerdConfigPatches:
 EOF
 
 # 2.remove taints
-controller_node=`kubectl get nodes --no-headers  -o custom-columns=NAME:.metadata.name| grep control-plane`
-kubectl taint nodes $controller_node node-role.kubernetes.io/master:NoSchedule-
+controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
+kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl get nodes -o wide
 
 # 3.install CNI
-kubectl apply -f calico.yaml
+kubectl apply -f ./calico.yaml
+
+# 4. wait all pods ready
+kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
+
