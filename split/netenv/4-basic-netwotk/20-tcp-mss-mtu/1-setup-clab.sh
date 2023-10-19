2fc619ca3ae7ba98a81338cd22684b7f7f59a8f5
rowan <olaf.luo@foxmail.com>
2023-10-19 20:28:10 +0800

    add mtu

--- split/netenv/4-basic-netwotk/20-tcp-mss-mtu/1-setup-clab.sh
+++ split/netenv/4-basic-netwotk/20-tcp-mss-mtu/1-setup-clab.sh
@@ -26,14 +26,14 @@ topology:
 
     server1:
       kind: linux
-      image: 192.168.2.100:5000/nettool
+      image: 192.168.2.100:5000/xcni
       exec:
       - ip addr add 10.1.5.10/24 dev net0
       - ip route replace default via 10.1.5.1
 
     server2:
       kind: linux
-      image: 192.168.2.100:5000/nettool
+      image: 192.168.2.100:5000/xcni
       exec:
       - ip addr add 10.1.8.10/24 dev net0
       - ip route replace default via 10.1.8.1
