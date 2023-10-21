01364f202ae2fc95fc05cd6522c6db5b305462d9
rowan <olaf.luo@foxmail.com>
2023-10-21 15:22:38 +0800

    add ztunnel

---
+++ split/ztunnel/1-setup-env.sh
@@ -0,0 +1,10 @@
+kind create cluster --image=kindest/node:v1.27.3 --config=- <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+name: ambient
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+EOF
+
