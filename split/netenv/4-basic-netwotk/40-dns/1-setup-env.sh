b2f5c171b0e99a2399b4b789b81ad45b31651b91
rowan <olaf.luo@foxmail.com>
2023-10-21 19:27:40 +0800

    mod dns

--- split/netenv/4-basic-netwotk/40-dns/1-setup-env.sh
+++ split/netenv/4-basic-netwotk/40-dns/1-setup-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -v
-#docker run --hostname dns --name dns --privileged --security-opt seccomp=unconfined --rm -td --tmpfs /tmp --tmpfs /run --volume /var --volume /lib/modules:/lib/modules:ro 192.168.2.100:5000/kindest/node:v1.27.3
+docker run --hostname dns --name dns --privileged --security-opt seccomp=unconfined --rm -td --tmpfs /tmp --tmpfs /run --volume /var --volume /lib/modules:/lib/modules:ro 192.168.2.100:5000/kindest/node:v1.27.3
 
-apt update && apt -y install bind9 dnsutils
+docker exec -it dns bash -c "apt update && apt -y install bind9 dnsutils"
 
 
