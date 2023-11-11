7380abff169f1e62bdbbcb35a72e60550dc0f115
rowan <olaf.luo@foxmail.com>
2023-11-11 16:03:37 +0800

    add keepalived

---
+++ split/netenv/4-adv-netwotk/80-L4-L7-LB/1-L4/3-keepalived/1-setup-clab.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -v
+
+{ ip l s brl4 down && brctl delbr brl4; } > /dev/null 2>&1
+brctl addbr brl4;ip l s brl4 up
+
+cat <<EOF>clab.yaml | clab deploy -t clab.yaml -
+name: l4lb
+mgmt:
+  ipv6-subnet: ""
+  ipv4-subnet: 172.20.20.0/24
+
+topology:
+  nodes:
+    brl4:
+      kind: bridge
+
+    keepalived1:
+      kind: linux
+      image: 192.168.2.100:5000/keepalived:2.0.8
+      exec:
+      - ip a a 10.1.5.99/24 dev net1
+      - keepalived -D  -f /etc/keepalived/keepalived.conf
+      cmd: sleep infinity 
+      binds:
+        - ./keepalived/keepalived1/keepalived.conf:/etc/keepalived/keepalived.conf
+
+    keepalived2:
+      kind: linux
+      image: 192.168.2.100:5000/keepalived:2.0.8
+      cmd: sleep infinity
+      exec:
+      - ip a a 10.1.5.101/24 dev net1
+      - keepalived -D  -f /etc/keepalived/keepalived.conf
+      binds:
+        - ./keepalived/keepalived2/keepalived.conf:/etc/keepalived/keepalived.conf
+
+    server1:
+      kind: linux
+      image: 192.168.2.100:5000/xcni
+      exec:
+      - ip addr add 10.1.5.10/24 dev net0
+      - ip route replace default via 10.1.5.1
+
+    server2:
+      kind: linux
+      image: 192.168.2.100:5000/xcni
+      exec:
+      - ip addr add 10.1.5.11/24 dev net0
+      - ip route replace default via 10.1.5.1
+
+
+  links:
+    - endpoints: ["server1:net0", "brl4:net1"]
+    - endpoints: ["server2:net0", "brl4:net2"]
+    - endpoints: ["keepalived1:net1", "brl4:net3"]
+    - endpoints: ["keepalived2:net1", "brl4:net4"]
+
+EOF
+
