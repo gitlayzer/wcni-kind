0c5227395194f6af29ce409350b0f69efa4398da
rowan <olaf.luo@foxmail.com>
2023-11-13 21:06:50 +0800

    haproxy && keepalived

--- split/netenv/4-adv-netwotk/80-L4-L7-LB/1-L4/3-keepalived/1-setup-clab.sh
+++ split/netenv/4-adv-netwotk/80-L4-L7-LB/1-L4/3-keepalived/1-setup-clab.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -v
 
-# topo:
-#          ---10.1.5.99/24-------------10.1.5.199
+# topo:    
+#                                        nginx
+#          ---10.1.5.99/24-------------10.1.5.80
 #          |                         /
-# client---| keepalived 10.1.5.100/24                 nginx
+# client---| keepalived 10.1.5.100/24  
 #          |                         \
-#          ---10.1.5.98/24-------------10.1.5.198
+#          ---10.1.5.98/24-------------10.1.5.90
+#                                        nginx
 
 { ip l s brl4 down && brctl delbr brl4; } > /dev/null 2>&1
 brctl addbr brl4;ip l s brl4 up
@@ -28,73 +30,57 @@ topology:
     brl4:
       kind: bridge
 
-    brl4l7:
-      kind: bridge
-
-    brl7:
-      kind: bridge
-
-    keepalived1:
+    keepalived1-haproxy1:
       kind: linux
-      image: 192.168.2.100:5000/keepalived:2.0.8
+      image: 192.168.2.100:5000/keepalived-2.0.8-haproxy-1.5.18
       exec:
       - ip a a 10.1.5.99/24 dev net1
       - keepalived -D  -f /etc/keepalived/keepalived.conf
+      - haproxy -f /etc/haproxy/haproxy.cfg
       cmd: sleep infinity 
       binds:
         - ./keepalived/keepalived1/keepalived.conf:/etc/keepalived/keepalived.conf
+        - ./haproxy/haproxy1/haproxy.cfg:/etc/haproxy/haproxy.cfg
 
-    keepalived2:
+    keepalived2-haproxy2:
       kind: linux
-      image: 192.168.2.100:5000/keepalived:2.0.8
+      image: 192.168.2.100:5000/keepalived-2.0.8-haproxy-1.5.18
       cmd: sleep infinity
       exec:
       - ip a a 10.1.5.101/24 dev net1
       - keepalived -D  -f /etc/keepalived/keepalived.conf
+      - haproxy -f /etc/haproxy/haproxy.cfg
       binds:
         - ./keepalived/keepalived2/keepalived.conf:/etc/keepalived/keepalived.conf
-
-    haproxy1:
-      kind: linux
-      image: 192.168.2.100:5000/haproxy:1.5.18
-      cmd: sleep infinity
-      exec:
-      - ip a a 10.1.5.199/24 dev net1
-      - haproxy -f /etc/haproxy/haproxy.cfg
-      binds:
-        - ./haproxy/haproxy1/haproxy.cfg:/etc/haproxy/haproxy.cfg
-
-    haproxy2:
-      kind: linux
-      image: 192.168.2.100:5000/haproxy:1.5.18
-      cmd: sleep infinity
-      exec:
-      - ip a a 10.1.5.201/24 dev net1
-      - haproxy -f /etc/haproxy/haproxy.cfg
-      binds:
         - ./haproxy/haproxy2/haproxy.cfg:/etc/haproxy/haproxy.cfg
 
-
-    server1:
+    nginx1:
       kind: linux
       image: 192.168.2.100:5000/xcni
       exec:
-      - ip addr add 10.1.5.10/24 dev net0
+      - ip addr add 10.1.5.80/24 dev net0
       - ip route replace default via 10.1.5.1
 
-    server2:
+    nginx2:
       kind: linux
       image: 192.168.2.100:5000/xcni
       exec:
-      - ip addr add 10.1.5.11/24 dev net0
+      - ip addr add 10.1.5.90/24 dev net0
+      - ip route replace default via 10.1.5.1
+
+    client:
+      kind: linux
+      image: 192.168.2.100:5000/xcni
+      exec:
+      - ip addr add 10.1.5.5/24 dev net0
       - ip route replace default via 10.1.5.1
 
 
   links:
-    - endpoints: ["server1:net0", "brl4:net1"]
-    - endpoints: ["server2:net0", "brl4:net2"]
-    - endpoints: ["keepalived1:net1", "brl4:net3"]
-    - endpoints: ["keepalived2:net1", "brl4:net4"]
-
+    - endpoints: ["nginx1:net0", "brl4:net1"]
+    - endpoints: ["nginx2:net0", "brl4:net2"]
+    - endpoints: ["keepalived1-haproxy1:net1", "brl4:net3"]
+    - endpoints: ["keepalived2-haproxy2:net1", "brl4:net4"]
+    - endpoints: ["client:net0", "brl4:net5"]
 EOF
 
