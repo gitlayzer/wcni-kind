d015927f0144ffb89deeb091f07da5024254eddb
rowan <olaf.luo@foxmail.com>
2023-11-22 20:48:49 +0800

    add tcp-server-ESTABLISH

--- split/netenv/4-adv-netwotk/30-tcp-3way-handshake/7-tcp-server-ESTABLISH/1-setup-clab.sh
+++ split/netenv/4-adv-netwotk/30-tcp-3way-handshake/7-tcp-server-ESTABLISH/1-setup-clab.sh
@@ -14,6 +14,7 @@ topology:
       exec:
       - ip a a 10.1.5.1/24 dev eth1
       - ip a a 10.1.8.1/24 dev eth2
+      - iptables -A FORWARD -s 10.1.5.10 -d 10.1.8.10 -p tcp --tcp-flags PSH,ACK PSH,ACK -j DROP
 
     server1:
       kind: linux
@@ -36,16 +37,27 @@ topology:
 EOF
 
 # 1.cmd:
-# n/a
+# iptables -A FORWARD -s 10.1.5.10 -d 10.1.8.10 -p tcp --tcp-flags PSH,ACK PSH,ACK -j DROP
 
 # 2.test
 # [root@server1 /]# curl 10.1.8.10 
 # ...  waiting...
 
-# 3. monitor netstat outputs[server1]
+# 3. monitor netstat outputs[server1]{keep into ESTABLISH status}
 [root@server1 /]# while true;do netstat -anp | grep EST;done
+# tcp        0     73 10.1.5.10:50396         10.1.8.10:80            ESTABLISHED 28865/curl          
+# tcp        0     73 10.1.5.10:50396         10.1.8.10:80            ESTABLISHED 28865/curl          
+# tcp        0     73 10.1.5.10:50396         10.1.8.10:80            ESTABLISHED 28865/curl          
+# tcp        0     73 10.1.5.10:50396         10.1.8.10:80            ESTABLISHED 28865/curl          
+# tcp        0     73 10.1.5.10:50396         10.1.8.10:80            ESTABLISHED 28865/curl          
+# tcp        0     73 10.1.5.10:50396         10.1.8.10:80            ESTABLISHED 28865/curl  
 
-# 3.1 monitor netstat outputs[server2]{come into ESTABLISH status}
+
+# 3.1 monitor netstat outputs[server2]{keep into ESTABLISH status}
 # [root@server2 /]# while true;do netstat -anp | grep ESTA;done         
-# tcp        0    236 10.1.8.10:80            10.1.5.10:39114         ESTABLISHED -  
-
+# tcp        0      0 10.1.8.10:80            10.1.5.10:50396         ESTABLISHED 15/nginx: worker pr 
+# tcp        0      0 10.1.8.10:80            10.1.5.10:50396         ESTABLISHED 15/nginx: worker pr 
+# tcp        0      0 10.1.8.10:80            10.1.5.10:50396         ESTABLISHED 15/nginx: worker pr 
+# tcp        0      0 10.1.8.10:80            10.1.5.10:50396         ESTABLISHED 15/nginx: worker pr 
+# tcp        0      0 10.1.8.10:80            10.1.5.10:50396         ESTABLISHED 15/nginx: worker pr 
+# tcp        0      0 10.1.8.10:80            10.1.5.10:50396         ESTABLISHED 15/nginx: worker pr 
