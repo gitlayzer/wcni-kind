aedbf92102c34770e9e6a5212ad8de6c173b4b39
rowan <olaf.luo@foxmail.com>
2023-12-05 21:41:39 +0800

    Adjust/Rename the Directory/File

--- split/netenv/4-adv-netwotk/90-eBPF/1-k3s-env-setup/1-kpr-setup-env-vmkp.sh
+++ split/netenv/4-adv-netwotk/90-eBPF/1-k3s-env-setup/1-kpr-setup-env-vmkp.sh
@@ -11,6 +11,7 @@ do
     - sudo echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config && echo 'root:hive' | sudo chpasswd && systemctl restart sshd
     - sudo mkdir -p /etc/rancher/k3s/ && wget http://192.168.2.100/k3s/registries.yaml -P /etc/rancher/k3s/
     - sudo wget -r -np -nH --cut-dirs=3 --directory-prefix=/opt/cni/bin/ http://192.168.2.100/k3s/cni/bin/ && find /opt/cni/bin/ -type f | xargs chmod +x
+    - sudo wget http://192.168.2.100/k3s/rzsz/rz -P /usr/bin/ && wget http://192.168.2.100/k3s/rzsz/sz -P /usr/bin/ && chmod +x /usr/bin/rz /usr/bin/sz
     - sudo bash -c '{ echo "alias all=\"kubectl get pods -A\""; echo "alias k=\"kubectl\""; echo "alias kk=\"kubectl -nkube-system\"" ; } >> ~/.bashrc'
 EOF
 done
@@ -27,7 +28,7 @@ for ((ip_id=0; ip_id<${#ip_addresses[@]}; ip_id++)); do
     k3s_version="v1.27.3+k3s1"
 
     if [ $ip_id -eq 0 ]; then
-	k3sup install --ip=$master_ip --user=root --merge --sudo --cluster --k3s-version=v1.27.3+k3s1 --k3s-extra-args "--flannel-backend=none --cluster-cidr=10.244.0.0/16 --disable-network-policy --disable-kube-proxy --disable traefik --disable servicelb --node-ip=$master_ip" --local-path $HOME/.kube/config --context=kpr
+	k3sup install --ip=$master_ip --user=root --merge --sudo --cluster --k3s-version=v1.27.3+k3s1 --k3s-extra-args "--flannel-backend=none --cluster-cidr=10.244.0.0/16 --disable-network-policy --disable-kube-proxy --disable traefik --disable servicelb --node-ip=$master_ip" --local-path $HOME/.kube/config --context=kprp
     else
         k3sup join --ip ${ip_addresses[$ip_id]} --user root --sudo --k3s-version=$k3s_version --server-ip $master_ip --server-user root
     fi
