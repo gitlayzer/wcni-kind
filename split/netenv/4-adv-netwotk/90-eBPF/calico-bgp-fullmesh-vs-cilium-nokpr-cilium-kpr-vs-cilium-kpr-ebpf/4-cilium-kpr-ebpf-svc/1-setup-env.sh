35d6a6700f102a24bdc5cf90eddb2d7996005967
rowan <olaf.luo@foxmail.com>
2023-12-08 21:46:53 +0800

    Adjust/Rename the Directory/File

--- split/netenv/4-adv-netwotk/90-eBPF/calico-bgp-fullmesh-vs-cilium-nokpr-cilium-kpr-vs-cilium-kpr-ebpf/4-cilium-kpr-ebpf-svc/1-setup-env.sh
+++ split/netenv/4-adv-netwotk/90-eBPF/calico-bgp-fullmesh-vs-cilium-nokpr-cilium-kpr-vs-cilium-kpr-ebpf/4-cilium-kpr-ebpf-svc/1-setup-env.sh
@@ -1,13 +1,19 @@
-$ file ds
+$ logical flow
+0. mv config.toml from /etc/containerd/
 mv /etc/containerd/config.toml /root/config.toml.bak
+
+1. kubeadm init
 systemctl restart containerd && systemctl enable containerd
+
 kubeadm config images pull --image-repository=registry.aliyuncs.com/google_containers
 kubeadm init --kubernetes-version=v1.27.3 --image-repository registry.aliyuncs.com/google_containers --pod-network-cidr=10.244.0.0/16 --service-cidr=10.96.0.0/12 --ignore-preflight-errors=Swap
 
+2. set private registry
 cat /etc/containerd/config.toml;cat ./config.toml
 
 # For existing installations with kube-proxy running as a DaemonSet, remove it by using the following commands below.Be aware that removing kube-proxy will break existing service connections. It will also stop service related traffic until the Cilium replacement has been installed.
 
+3.delete kube-proxy
 $ kubectl -n kube-system delete ds kube-proxy
 $ # Delete the configmap as well to avoid kube-proxy being reinstalled during a Kubeadm upgrade (works only for K8s 1.19 and newer)
 $ kubectl -n kube-system delete cm kube-proxy
